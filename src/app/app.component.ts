53ba6195e8a1cd4123ac68c86880698f52c14b20
Maksymilian Paluszek <maksymilian.paluszek@ceneo.pl>
2023-09-29 19:03:39 +0200

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  opened: boolean = false;
+  @ViewChild('sidenav') sidenav!: MatSidenav;
+
   title = 'magiczna-flet';
+
+  sideClick(): void {
+    this.sidenav.toggle();
+  }
 }
