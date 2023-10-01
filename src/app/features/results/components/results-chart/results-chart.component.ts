b666ecfe42ab51743a2947ccf1bbf3867558ec37
Ryszard Raby <ryszard.raby@ceneo.pl>
2023-10-01 08:52:44 +0200

    gfx

--- src/app/features/results/components/results-chart/results-chart.component.ts
+++ src/app/features/results/components/results-chart/results-chart.component.ts
@@ -11,8 +11,20 @@ export class ResultsChartComponent implements OnInit {
   @Input() results!: MatchResult[];
 
   public radarChartOptions: ChartConfiguration<'radar'>['options'] = {
-    responsive: true
+    responsive: true,
+    elements: {
+      line: {
+        borderColor: '#3cba9f',
+      },
+      point: {
+        radius: 5,
+        borderWidth: 3,
+        borderColor: '#3cba9f',
+        backgroundColor: '#fff',
+      }
+    },
   };
+
   public radarChartLabels!: string[];
 
   public radarChartData!: ChartConfiguration<'radar'>['data']['datasets'];
@@ -23,7 +35,14 @@ export class ResultsChartComponent implements OnInit {
     this.radarChartLabels = this.results.map(result => result.name);
     const data = this.results.map(result => result.percentageMatch);
     this.radarChartData = [
-      { data: data, label: 'Dopasowanie' },
-    ];;
+      { data: data,
+        label: 'Dopasowanie',
+        borderColor: '#3cba9f',
+        pointBorderColor: '#3cba9f',
+        backgroundColor: 'rgba(255, 255, 255, 0.2)', // Kolor wypełnienia
+        pointHoverBackgroundColor: '#fff', // Kolor punktów podczas najazdu myszką
+        pointHoverBorderColor: 'rgba(255, 255, 255, 1)', // Kolor obramowania punktów podczas najazdu myszką
+      },
+    ];
   }
 }
